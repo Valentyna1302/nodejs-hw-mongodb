05cb9d31fe2fb5aa1c7c060349c0daea8afc37ff
Valentyna1302 <122233669+Valentyna1302@users.noreply.github.com>
2025-04-15 13:53:58 +0200

    added sort

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,36 +1,36 @@
-import { HttpError } from 'http-errors';
-
-export const errorHandler = (err, req, res, next) => {
-  if (err instanceof HttpError) {
-    res.status(err.status).json({
-      status: err.status,
-      message: err.name,
-      data: err,
-    });
-    return;
-  }
-
-  res.status(500).json({
-    status: 500,
-    message: 'Something went wrong',
-    data: err.message,
-  });
-};
 // import { HttpError } from 'http-errors';
 
-// export const errorHandler = (error, req, res, next) => {
-//   if (error instanceof HttpError) {
-//     return res.status(error.status).json({
-//       status: error.status,
-//       message: error.message,
+// export const errorHandler = (err, req, res, next) => {
+//   if (err instanceof HttpError) {
+//     res.status(err.status).json({
+//       status: err.status,
+//       message: err.name,
+//       data: err,
 //     });
+//     return;
 //   }
 
-//   const status = error.status || 500;
-//   const message = error.message || 'Server error';
-
-//   res.status(status).json({
-//     status,
-//     message,
+//   res.status(500).json({
+//     status: 500,
+//     message: 'Something went wrong',
+//     data: err.message,
 //   });
 // };
+import { HttpError } from 'http-errors';
+
+export const errorHandler = (error, req, res, next) => {
+  if (error instanceof HttpError) {
+    return res.status(error.status).json({
+      status: error.status,
+      message: error.message,
+    });
+  }
+
+  const status = error.status || 500;
+  const message = error.message || 'Server error';
+
+  res.status(status).json({
+    status,
+    message,
+  });
+};
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,30 @@
+import { SORT_ORDER } from '../constants/index.js';
 import { ContactsColection } from '../db/models/Contacts.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async () => {
-  const contacts = await ContactsColection.find();
-  return contacts;
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+}) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = ContactsColection.find();
+  const contactsCount = await ContactsColection.find()
+    .merge(contactsQuery)
+    .countDocuments();
+
+  const contacts = await contactsQuery
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder })
+    .exec();
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return { data: contacts, ...paginationData };
 };
 
 export const getContactById = async (contactId) => {
@@ -19,7 +41,7 @@ export const updateContact = async (contactId, payload, options = {}) => {
   const rawResult = await ContactsColection.findOneAndUpdate(
     { _id: contactId },
     payload,
-    { includeResultMetadata: true, ...options },
+    { ...options },
   );
   return rawResult;
 };
