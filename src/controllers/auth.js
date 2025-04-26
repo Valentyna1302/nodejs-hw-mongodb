cfb14c521d03c087d73d2f4cc54032fc2311132e
Valentyna1302 <122233669+Valentyna1302@users.noreply.github.com>
2025-04-26 12:18:19 +0200

    reset password

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,6 +5,7 @@ import {
   refreshUsersSession,
   registerUser,
   requestResetToken,
+  resetPassword,
 } from '../services/auth.js';
 
 export const registerUserController = async (req, res) => {
@@ -80,7 +81,16 @@ export const refreshUserSessionController = async (req, res) => {
 export const requestResetEmailController = async (req, res) => {
   await requestResetToken(req.body.email);
   res.json({
-    message: 'Reset password email was successfully sent!',
+    message: 'Reset password email has been successfully sent.',
+    status: 200,
+    data: {},
+  });
+};
+
+export const resetPasswordController = async (req, res) => {
+  await resetPassword(req.body);
+  res.json({
+    message: 'Password has been successfully reset.',
     status: 200,
     data: {},
   });
